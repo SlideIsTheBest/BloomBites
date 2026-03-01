49967ce9f2044e9f9cebafa2e99061d398f9a2f5
Slide <slide2333rblx@gmail.com>
2026-03-01 11:08:45 +0700

    Update Timer.js

--- Timer.js
+++ Timer.js
@@ -5,7 +5,7 @@ const preorder = document.getElementById("preorder");
 var preorder_allow = false;
 
 const start_date = new Date(2026, 2, 1, 0, 0, 0).getTime();  // Month starts with 0 (January)
-const end_date = new Date(2026, 2, 9, 0, 0, 0).getTime();
+const end_date = new Date(2026, 2, 8, 12, 0, 0).getTime();
 
 const countdown = setInterval(() => {
   const now = new Date().getTime();
@@ -71,5 +71,6 @@ const countdown = setInterval(() => {
     }; 
   };
 }, 1000);
+
 
 
